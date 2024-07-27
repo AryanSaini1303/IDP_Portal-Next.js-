3d270c9cf5d489fa46b6663f492190f3f4d53bf7
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-07-27 23:31:15 +0530

    Feat: student data fetched after login and displayed on landing page

--- app/LandingPage/[session]/page.js
+++ app/LandingPage/[session]/page.js
@@ -11,11 +11,32 @@ const rubik = Rubik({
 
 export default function LandingPage({ params }) {
   const [session, setSession] = useState();
+  const [studentData, setStudentData] = useState();
   const router = useRouter();
 
   useEffect(() => {
     setSession(JSON.parse(decodeURIComponent(params.session)));
   }, [params]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (session && session.user && session.user.email) {
+        try {
+          const response = await fetch(
+            `/api/getCurrentStudent?email=${session.user.email}`
+          );
+          if (!response.ok) {
+            throw new Error(`Error fetching data: ${response.statusText}`);
+          }
+          const data = await response.json();
+          setStudentData(data);
+        } catch (err) {
+          console.error("Error fetching student data:", err);
+        }
+      }
+    };
+    fetchData();
+  }, [session]);
 
   if (!session) {
     return <p>Loading...</p>;
@@ -32,26 +53,22 @@ export default function LandingPage({ params }) {
                 signOut({ callbackUrl: "/" }).then(() => {
                   router.push("/");
                 });
-                {
-                  /*Relying solely on callbackUrl might not always guarantee redirection due to potential issues with next-auth's internal handling. Using only router.push('/') might bypass next-auth's built-in sign-out process, which could lead to inconsistencies. Combining both ensures the user is redirected reliably while also leveraging next-auth's built-in functionality.
-                  normally callbackUrl should handle the redirection after sign-out if next-auth is functioning correctly. It is designed to direct users to the specified route once the sign-out process completes. However, using router.push('/') as a fallback ensures that redirection happens even if there are issues with next-auth's handling, providing a more reliable user experience. */
-                }
               }}
             >
               <svg
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      viewBox="0 0 24 24"
-      height="1em"
-      width="1em"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" />
-      <path d="M14 8V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2h7a2 2 0 002-2v-2" />
-      <path d="M7 12h14l-3-3m0 6l3-3" />
-    </svg>
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+                height="1em"
+                width="1em"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <path d="M14 8V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2h7a2 2 0 002-2v-2" />
+                <path d="M7 12h14l-3-3m0 6l3-3" />
+              </svg>
             </button>
           </div>
         </abbr>
@@ -59,37 +76,39 @@ export default function LandingPage({ params }) {
         <img src={session.user.image} alt="User's Image" />
       </div>
       <div className="content">
-        <div className="card">
-          <div className="student_info">
-            <ul>
-              <li className="holder">Name:</li>
-              <li className="value">{session.user.name}</li>
-              <li className="holder">Admission:</li>
-              <li className="value">admission</li>
-              <li className="holder">Enrollment:</li>
-              <li className="value">enrollment</li>
-              <li className="holder">Program:</li>
-              <li className="value">program</li>
-              <li className="holder">School:</li>
-              <li className="value">school</li>
-              <li className="holder">Contact:</li>
-              <li className="value">contact</li>
-              <li className="holder">Email:</li>
-              <li className="value">{session.user.email}</li>
-            </ul>
-          </div>
-          <div className="main">
-            <h1>Choose Your Category</h1>
-            <div className="options">
-              <a href="/Research">
-                <button className={rubik.className}>Research</button>
-              </a>
-              <a href="/Business">
-                <button className={rubik.className}>Business</button>
-              </a>
+        {studentData ? (
+          <div className="card">
+            <div className="student_info">
+              <ul>
+                <li className="holder">Name:</li>
+                <li className="value">{session.user.name}</li>
+                <li className="holder">Admission:</li>
+                <li className="value">{studentData.admissionNum}</li>
+                <li className="holder">Enrollment:</li>
+                <li className="value">{studentData.enrollmentNum}</li>
+                <li className="holder">Program:</li>
+                <li className="value">{studentData.program}</li>
+                <li className="holder">School:</li>
+                <li className="value">{studentData.school}</li>
+                <li className="holder">Contact:</li>
+                <li className="value">{studentData.contact}</li>
+                <li className="holder">Email:</li>
+                <li className="value">{session.user.email}</li>
+              </ul>
+            </div>
+            <div className="main">
+              <h1>Choose Your Category</h1>
+              <div className="options">
+                <a href="/Research">
+                  <button className={rubik.className}>Research</button>
+                </a>
+                <a href="/Business">
+                  <button className={rubik.className}>Business</button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        ):<h1>Loading...</h1>}
       </div>
       <footer>
         <p>&copy; 2024 IDP Registration Portal. All rights reserved.</p>
---
+++ pages/api/getCurrentStudent.js
@@ -0,0 +1,17 @@
+import prisma from "@/lib/prisma";
+import NodeCache from "node-cache";
+
+const cache = new NodeCache({ stdTTL: 0, checkperiod: 0 });
+export default async function getCurrentStudent(req, res) {
+    const email = req.query.email;
+    const cachedData = cache.get(email);
+  console.log(email);
+  if (cachedData) {
+    return res.status(200).json(cachedData);
+  }
+  const data = await prisma.student.findUnique({
+    where: { email: email },
+  });
+  cache.set(email, data);
+  res.status(200).json(data);
+}
